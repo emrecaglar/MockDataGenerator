39b7a39875707486cb473a47cdde005db12e2acc
Emre ÇAĞLAR <emre.caglar@ecozum.com>
2019-11-14 16:39:37 +0300

    add AutoIncrementDataGenerator

---
+++ src/Mocking.DataGenerator/Generators/AutoIncrementDataGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mocking.DataGenerator.Generators
+{
+    public class AutoIncrementDataGenerator : IDataGenerator<int>
+    {
+        private readonly int _start;
+        private readonly int _increment;
+
+        private int _current;
+
+        public AutoIncrementDataGenerator(int start = 1, int increment = 1)
+        {
+            _increment = increment;
+            _start = start;
+
+            _current = _start;
+        }
+
+        public int Get()
+        {
+            int data = _current;
+
+            _current += _increment;
+
+            return data;
+        }
+    }
+}
---
+++ test/Mocking.DataGenerator.Tests/AutoIncrementTests.cs
@@ -0,0 +1,31 @@
+﻿using Mocking.DataGenerator.Generators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Mocking.DataGenerator.Tests
+{
+    public class AutoIncrementTests
+    {
+        [Fact]
+        public void AutoIncrement_ShoulBeIncrement()
+        {
+            var data = new MockDataGenerator<MyType>()
+                            .Register(x => x.Value, new AutoIncrementDataGenerator(start:10, increment: 10))
+                            .Generate(count:5);
+
+
+            Assert.Equal(10, data[0].Value);
+            Assert.Equal(20, data[1].Value);
+            Assert.Equal(30, data[2].Value);
+            Assert.Equal(40, data[3].Value);
+            Assert.Equal(50, data[4].Value);
+        }
+    }
+
+    public class MyType
+    {
+        public int Value { get; set; }
+    }
+}
