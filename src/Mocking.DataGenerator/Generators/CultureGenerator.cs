7b1ca1e0a27ffb9309bf9202f130e9d7e581b3cb
Emre Caglar <emre.caglar@ecozum.teknik>
2019-12-19 14:34:34 +0300

    add culture info generator

---
+++ src/Mocking.DataGenerator/Generators/CultureGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Mocking.DataGenerator.Generators
+{
+    public class CultureGenerator : RandomizerBase, IDataGenerator<string>
+    {
+        public string Get()
+        {
+            var cultures = CultureInfo.GetCultures(CultureTypes.NeutralCultures)
+                                      .Select(x => x.Name)
+                                      .ToArray();
+
+            return cultures[Randomizer.Next(0, cultures.Length - 1)];
+        }
+    }
+}
