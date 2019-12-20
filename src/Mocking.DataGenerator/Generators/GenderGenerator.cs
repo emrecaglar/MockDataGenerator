5bf88f4d971986554c6ff14590443a59304eb1a1
Emre Caglar <emre.caglar@ecozum.teknik>
2019-12-20 09:18:46 +0300

    add data generators

---
+++ src/Mocking.DataGenerator/Generators/GenderGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mocking.DataGenerator.Generators
+{
+    public class GenderGenerator : RandomizerBase, IDataGenerator<string>
+    {
+        private readonly string[] _genders = new[] { "Male", "Female", "Unknown" };
+
+        public string Get()
+        {
+            return _genders[Randomizer.Next(0, _genders.Length - 1)];
+        }
+    }
+}
---
+++ src/Mocking.DataGenerator/Generators/RegionGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Mocking.DataGenerator.Generators
+{
+    public class RegionGenerator : RandomizerBase, IDataGenerator<string>
+    {
+        private readonly CultureInfo[] _cultures = CultureInfo.GetCultures(CultureTypes.NeutralCultures).ToArray();
+
+        public string Get()
+        {
+            return new RegionInfo(_cultures[Randomizer.Next(0, _cultures.Length)].LCID).DisplayName;
+        }
+    }
+}
