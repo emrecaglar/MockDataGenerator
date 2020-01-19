f40bd91a674e1d072eb1fd95301d625b07c506a4
Emre Caglar <emre.caglar@ecozum.teknik>
2020-01-19 15:57:12 +0300

    add new mock data generators

---
+++ src/Mocking.DataGenerator/Generators/PhoneGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mocking.DataGenerator.Generators
+{
+    public class PhoneGenerator : RandomizerBase, IDataGenerator<string>
+    {
+        private readonly string _format;
+
+        public PhoneGenerator(string format = null)
+        {
+            _format = format;
+        }
+             
+        public string Get()
+        {
+            int countryCode = Randomizer.Next(1, 9);
+            int digits = Randomizer.Next(349586710, 876586959);
+            int lastDigit = Randomizer.Next(0, 9);
+
+            long phone;
+            if (_format.Contains('+'))
+            {
+                phone = long.Parse($"{countryCode}{digits}{lastDigit}");
+            }
+            else
+            {
+                phone = long.Parse($"{digits}{lastDigit}");
+            }
+            
+            return phone.ToString(_format);
+        }
+    }
+}
---
+++ test/Mocking.DataGenerator.Tests/PhoneNumberTests.cs
@@ -0,0 +1,20 @@
+﻿using Mocking.DataGenerator.Generators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Mocking.DataGenerator.Tests
+{
+    public class PhoneNumberTests
+    {
+        [Fact]
+        public void PhoneNumberGenerator_ShoulBePhoneGenerate()
+        {
+            var generator = new PhoneGenerator(format: "+#(###)-###-##-##");
+            var phone = generator.Get();
+
+            Assert.NotNull(phone);
+        }
+    }
+}
